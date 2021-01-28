a23ba44330b433c490f712e292bf9e88d6b53bfd
Tymoteusz Perka <01143584@pw.edu.pl>
2021-01-28 17:03:44 +0100

    GUI mockup update

---
+++ MainMenu.java
@@ -0,0 +1,88 @@
+/*
+ * Created by JFormDesigner on Thu Jan 07 14:08:18 CET 2021
+ */
+
+package bd2.gui;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+import com.jgoodies.forms.factories.*;
+import com.jgoodies.forms.layout.*;
+
+/**
+ * @author Tymoteusz Perka
+ */
+public class MainMenu extends JPanel {
+    private JFrame frame;
+    public MainMenu(JFrame frame) {
+        this.frame = frame;
+        initComponents();
+        frame.add(this);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    private void initComponents() {
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
+        // Generated using JFormDesigner Evaluation license - Tymoteusz Perka
+        titleLabel = new JLabel();
+        buySingleTicketButton = new JButton();
+        buySeasonTicketButton = new JButton();
+        myReservationsTicketButton = new JButton();
+        myTicketsButton = new JButton();
+
+        //======== this ========
+        setBorder ( new javax . swing. border .CompoundBorder ( new javax . swing. border .TitledBorder ( new javax . swing. border .EmptyBorder ( 0
+        , 0 ,0 , 0) ,  "JF\u006frmDes\u0069gner \u0045valua\u0074ion" , javax. swing .border . TitledBorder. CENTER ,javax . swing. border .TitledBorder . BOTTOM
+        , new java. awt .Font ( "D\u0069alog", java .awt . Font. BOLD ,12 ) ,java . awt. Color .red ) ,
+         getBorder () ) );  addPropertyChangeListener( new java. beans .PropertyChangeListener ( ){ @Override public void propertyChange (java . beans. PropertyChangeEvent e
+        ) { if( "\u0062order" .equals ( e. getPropertyName () ) )throw new RuntimeException( ) ;} } );
+        setLayout(new FormLayout(
+            "4*(default, $lcgap), 80dlu, 2*(0dlu), 100dlu, $lcgap, 80dlu, 9*($lcgap, default)",
+            "29*(default, $lgap), default"));
+
+        //---- titleLabel ----
+        titleLabel.setText("Polskie Koleje Pa\u0144stwowe");
+        titleLabel.setFont(titleLabel.getFont().deriveFont(titleLabel.getFont().getSize() + 20f));
+        add(titleLabel, CC.xywh(9, 5, 6, 1, CC.CENTER, CC.CENTER));
+
+        //---- buySingleTicketButton ----
+        buySingleTicketButton.setText("Kup bilet jednorazowy");
+        buySingleTicketButton.setBackground(new Color(255, 128, 0));
+        add(buySingleTicketButton, CC.xy(12, 9));
+
+        //---- buySeasonTicketButton ----
+        buySeasonTicketButton.setText("Kup bilet okresowy");
+        buySeasonTicketButton.setBackground(new Color(255, 128, 0));
+        add(buySeasonTicketButton, CC.xy(12, 13));
+
+        //---- myReservationsTicketButton ----
+        myReservationsTicketButton.setText("Moje rezerwacje");
+        myReservationsTicketButton.setBackground(new Color(255, 128, 0));
+        add(myReservationsTicketButton, CC.xy(12, 17, CC.DEFAULT, CC.CENTER));
+
+        //---- myTicketsButton ----
+        myTicketsButton.setText("Moje bilety");
+        myTicketsButton.setBackground(new Color(255, 128, 0));
+        add(myTicketsButton, CC.xy(12, 21, CC.DEFAULT, CC.CENTER));
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents
+        buySingleTicketButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.getContentPane().removeAll();
+                new SearchMenu(frame);
+            }
+        });
+    }
+
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
+    // Generated using JFormDesigner Evaluation license - Tymoteusz Perka
+    private JLabel titleLabel;
+    private JButton buySingleTicketButton;
+    private JButton buySeasonTicketButton;
+    private JButton myReservationsTicketButton;
+    private JButton myTicketsButton;
+    // JFormDesigner - End of variables declaration  //GEN-END:variables
+}
---
+++ db/DbManager.java
@@ -0,0 +1,40 @@
+package bd2.gui.db;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.GregorianCalendar;
+import java.util.List;
+
+public class DbManager {
+
+    Connection con;
+
+    public DbManager() {
+        try {
+            DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+            con = DriverManager.getConnection(DbSettings.url, DbSettings.username, DbSettings.password);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+    public List<String> getStations() throws SQLException {
+        PreparedStatement st = con.prepareStatement("SELECT * FROM stacja");
+
+        List<String> result = new ArrayList<>();
+        ResultSet rs = st.executeQuery();
+        while(rs.next()) {
+            result.add(rs.getString(2));
+        }
+
+        st.close();
+        rs.close();
+        return result;
+    }
+
+    public List<Object> getRides(String startStation, String endStation, boolean isDeparture, GregorianCalendar date) {
+
+
+        return null;
+
+    }
+}
